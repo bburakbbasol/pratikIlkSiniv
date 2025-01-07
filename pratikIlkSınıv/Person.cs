5496c4f08c09e1525005bca0ac04c21c58854474
bburakbbasol <bburakbbasol@gmail.com>
2025-01-07 13:34:09 +0300

    Add project files.

---
+++ pratikIlkSınıv/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratikIlkSınıv
+{
+	public class Person
+	{
+		// Kişinin adını, soyadını ve doğum tarihini saklayan özel alanlar
+		private string name;
+		private string surname;
+		private int date;
+
+		// 'Name' özelliği için getter ve setter
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		// 'Surname' özelliği için getter ve setter
+		public string Surname
+		{
+			get { return surname; }
+			set { surname = value; }
+		}
+
+		// 'Date' özelliği için getter ve setter (Doğum tarihi için doğrulama yapılır)
+		public int Date
+		{
+			get { return date; }
+			set
+			{
+				if (value > 0)
+					date = value;
+			}
+		}
+
+		// Titla göre bilgi oluşturup ekrana yazdıran metod
+		public void Create(string title)
+		{
+			if (title == "öğrenci")
+				Console.WriteLine($"{name} adında {surname} soyadında {date} doğumlu öğrenci oluşturuldu ");
+			else if (title == "öğretmen")
+				Console.WriteLine($"{name} adında {surname} soyadında {date} doğumlu öğretmen oluşturuldu ");
+			else
+				Console.WriteLine("Oluşturmak istediğiniz yapıyı doğru seçin");
+		}
+	}
+}
---
+++ pratikIlkSınıv/Program.cs
@@ -0,0 +1,24 @@
+﻿using pratikIlkSınıv; // Person sınıfını içeren özel namespace
+using System;
+
+class program
+{
+	static void Main()
+	{
+		// Bir öğrenci nesnesi oluşturuluyor ve değerler atanıyor
+		Person ogrenci = new Person();
+		ogrenci.Name = "Burak";
+		ogrenci.Surname = "Başol";
+		ogrenci.Date = 1999;
+		string title = "öğrenci";
+		ogrenci.Create(title);
+
+		// Bir öğretmen nesnesi oluşturuluyor ve değerler atanıyor
+		Person ogretmen = new Person();
+		ogretmen.Name = "Mehmet";
+		ogretmen.Surname = "Turgut";
+		ogretmen.Date = 1978;
+		title = "öğretmen";
+		ogretmen.Create(title);
+	}
+}
